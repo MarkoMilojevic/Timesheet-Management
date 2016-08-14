4b450fbf1a3643b8599c99e07f94ddfeb05a7fda
Marko Milojevic <milojevicmarko92@gmail.com>
2016-08-14 22:21:36 +0200

    Refactor Data layer

--- TimesheetManagement.Data/Repositories/IRepository.cs
+++ TimesheetManagement.Data/Repositories/IRepository.cs
@@ -3,26 +3,26 @@ using TimesheetManagement.Business.Entities;
 
 namespace TimesheetManagement.Data.Repositories
 {
-    public interface IRepository
-    {
-        Client GetClient(string tin);
+	public interface IRepository
+	{
+		Client GetClient(string tin);
 
-        ICollection<Client> GetClients();
+		ICollection<Client> GetClients();
 
-        Project GetProject(int id);
+		Project GetProject(int id);
 
-        ICollection<Project> GetProjects();
+		ICollection<Project> GetProjects();
 
-        Activity GetActivity(int id);
+		Activity GetActivity(int id);
 
-        ICollection<Activity> GetActivities(int projectId);
+		ICollection<Activity> GetActivities(int projectId);
 
-        Employee GetEmployee(int id);
+		Employee GetEmployee(int id);
 
-        Employee GetEmployee(string email);
+		Employee GetEmployee(string email);
 
-        ICollection<Employee> GetEmployees();
+		ICollection<Employee> GetEmployees();
 
-        ICollection<EmployeeActivity> GetEmployeeActivities(int employeeId);
-    }
+		ICollection<EmployeeActivity> GetEmployeeActivities(int employeeId);
+	}
 }
